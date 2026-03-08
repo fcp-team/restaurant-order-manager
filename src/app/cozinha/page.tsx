4f5005f5f2e7d131999766960ac8f9d7630a983f
StephannyBorrozzino <154994245+StephannyBorrozzino@users.noreply.github.com>
2026-03-08 10:00:23 -0300

    feat(view): adiciona a interface da cozinha

---
+++ src/app/cozinha/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import Header from "@/components/Header";
+import CardPedidoAction from "@/components/CardPedidoAction";
+
+export default function Cozinha() {
+  return (
+    <>
+     <Header/>
+    <h2 className="text-3xl m-8">Pedidos Em Andamento</h2>
+    <div className="flex flex-row gap-10 flex-wrap justify-center items-center">
+      {/* TODO: pegar os pedidos em andamento e colocar em um map com CardPedidoAction */}
+    </div>
+    </>
+  );
+}
