94254db26998831f720774e8689285839fecbb57
André Alves <andre.fortes.alves06@gmail.com>
2026-03-16 11:38:58 -0300

    feat: adiciona um serviodor HTTP para o WebSocket e a função de broadcast

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,7 @@
-"use client"
-
-import { getSocket } from "@/lib/ws-client"
-
 import styles from "./page.module.css"
 
-const socket = getSocket()
-
-socket.addEventListener("open", () => console.log("WebSocket connection established"))
-
-socket.addEventListener("message", (event) => {
-  console.log("Received message from server: ", JSON.parse(event.data))
-})
-
 export default function Home() {
-
-  function sendMessage() {
-    const event = {
-      type: "new_order",
-      restaurantId: 1,
-      orderId: 3
-    }
-
-    socket.send(JSON.stringify(event))
-  }
-
   return (
-    <>
-      <button type="button" onClick={sendMessage}>Enviar mensagem</button>
-    </>
+    <></>
   )
 }
