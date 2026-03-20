3c5c3a5d84b1a4a3eb88c348c8bef6716f7fc01c
Stephanny Borrozzino <154994245+StephannyBorrozzino@users.noreply.github.com>
2026-03-20 08:24:54 -0300

    feat(view): adiciona interface da cozinha e componentes (#3)
    * feat(components): adiciona componentes header, cards e avatar
    
    * feat(view): adiciona a interface da cozinha
    
    * feat(components): adiciona o componente CardPedidoView
    
    * substituição do componente CardPedidoAction para CardPedidoCozinha
    
    * correção no map, de pedido.itens para itens
    
    * refactor(database): atualiza estrutura do banco com restaurantes, enderecos e triggers
    
    * Adição do WebSocket (#6)
    
    * feat: initialize database schema and configure the MySQL connection
    
    * feat: add order service and order repository
    
    * feat: add WebSocket server and test connection to the client
    
    * feat: adiciona um serviodor HTTP para o WebSocket e a função de broadcast
    
    * feat(components): adiciona componentes header, cards e avatar
    
    * feat(view): adiciona a interface da cozinha
    
    * feat(components): adiciona o componente CardPedidoView
    
    * substituição do componente CardPedidoAction para CardPedidoCozinha
    
    * correção no map, de pedido.itens para itens
    
    * ajustes solicitados na função mudarEstado do CardPedidoCozinha
    
    ---------
    
    Co-authored-by: ReinaldoRoque <reinaldo.roque91@gmail.com>
    Co-authored-by: André Alves <112015245+andre-f-alves@users.noreply.github.com>

---
+++ src/components/CardPedidoCozinha.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react"
+
+type CardCozinhaProps = {
+    pedido: Record<string, any>
+}
+
+export default function CardPedidoCozinha({pedido}: CardCozinhaProps){
+    
+     const [itens, setItens] = useState(pedido.itens)
+
+    function mudarEstado() {
+       
+            const novosItens = itens.map((item: any) => ({
+            ...item,
+            status: "Finalizado"
+        }))
+
+        setItens(novosItens);
+
+        const payload = {
+        itens: novosItens.map((item: any) => ({
+            idItemPedido: item.id, 
+            novoStatus: item.status
+        }))
+        };
+
+         fetch("/api/pedido/itens/atualizar-status", {method: 'PATCH',headers: {"Content-Type": 'application/json'}, body: JSON.stringify(payload)})
+      
+    }
+
+    return (
+        <div style={{width: 430}} className="bg-[var(--color-surface)] border-[var(--color-surface-border)] border-[var(--color-button-action-border)] border-2 flex flex-col rounded-2xl p-4 m-4">
+            <h3 className="text-4xl">Pedido n°{pedido.numeroPedido} - Mesa {pedido.numeroMesa}</h3>
+            <p className="pt-2">{pedido.data}</p>
+            <ul className="text-2xl list-disc list-inside m-3">
+                {itens?.map((item: string, index: number) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
+            <button onClick={mudarEstado} className="cursor-pointer bg-[var(--color-button-action)] transition duration-300 hover:bg-[var(--color-button-action-hover)] border-[var(--color-button-action-border)] border-2 rounded-2xl p-2 px-5 m-3">
+              Mudar Estado
+            </button>
+        </div>
+    )
+}
