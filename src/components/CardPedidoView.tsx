d7edac9141fde5923957490a0e400e3ce6ddd826
StephannyBorrozzino <154994245+StephannyBorrozzino@users.noreply.github.com>
2026-03-21 20:39:25 -0300

    adição da interface do admin, pedidos e convidar membro

--- src/components/CardPedidoView.tsx
+++ src/components/CardPedidoView.tsx
@@ -1,19 +1,15 @@
 type CardViewProps = {
-    numeroPedido: string,
-    numeroMesa: string,
-    data: string,
-    itens: string[],
-    estado: string
+    pedido: Record<string, any>
 }
 
-export default function CardPedidoView({numeroPedido, numeroMesa, data, itens, estado}: CardViewProps){
+export default function CardPedidoView({pedido}: CardViewProps){
     return (
         <div style={{width: 430}} className="bg-surface border-[var(--color-surface-border)] border-[var(--color-button-action-border)] border-2 flex flex-col rounded-2xl p-4 m-4">
-            <h3 className="text-4xl">Pedido n°{numeroPedido} - Mesa {numeroMesa}</h3>
-            <p className="pt-2">{data}</p>
-            <p className="pt-2">{estado}</p>
+            <h3 className="text-4xl">Pedido n°{pedido.numeroPedido} - Mesa {pedido.numeroMesa}</h3>
+            <p className="pt-2">{pedido.data}</p>
+            <p className="pt-2">{pedido.status}</p>
             <ul className="text-2xl list-disc list-inside m-3">
-                {itens.map((item, index) => (
+                {pedido.itens?.map((item: string, index: number) => (
                     <li key={index}>{item}</li>
                 ))}
             </ul>
