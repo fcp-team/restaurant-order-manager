63dbbc368293010da7fc7d2cf171923e054d5f65
StephannyBorrozzino <154994245+StephannyBorrozzino@users.noreply.github.com>
2026-03-22 16:22:59 -0300

    troca de rota no botão de voltar

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
     <div className="text-center mt-10">
       <h1 className="text-[var(--color-text-primary)] text-7xl">404</h1>
       <p className="text-[var(--color-text-primary)] text-3xl mt-4 mb-9">Página não encontrada!</p>
-      <Link href="/" className="cursor-pointer bg-[var(--color-button-action)] transition duration-300 hover:bg-[var(--color-button-action-hover)] border-[var(--color-button-action-border)] border-2 rounded-2xl p-2 px-5 mt-5">Voltar para a Home</Link>
+      <Link href="/login" className="cursor-pointer bg-[var(--color-button-action)] transition duration-300 hover:bg-[var(--color-button-action-hover)] border-[var(--color-button-action-border)] border-2 rounded-2xl p-2 px-5 mt-5">Voltar para a Home</Link>
     </div>
   );
 }
