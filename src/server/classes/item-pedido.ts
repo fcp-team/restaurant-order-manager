a0d785d7ee4edd7c555fdafdaf289ebcd51b2539
André Alves <andre.fortes.alves06@gmail.com>
2026-03-23 12:18:46 -0300

    feat: ajuste nos getters e setters do membros de Pedido e de ItemPedido

--- src/server/classes/item-pedido.ts
+++ src/server/classes/item-pedido.ts
@@ -5,53 +5,56 @@ export enum StatusItemPedido {
 }
 
 export class ItemPedido {
-  
-  constructor(
-    private id: string,
-    private readonly idItemMenu: string,
-    private readonly nome: string,
-    private quantidade: number,
-    private readonly precoUnitario: number,
-    private observacao?: string,
-    private status: StatusItemPedido = StatusItemPedido.PENDENTE
-  ) {}
+  private id: string | null = null
+  private nome!: string
+  private precoUnitario!: number
+  private status: StatusItemPedido = StatusItemPedido.PENDENTE
 
-  get Id() {
-    return this.id;
-  }
+  constructor(
+    private readonly idItemMenu: string,
+    private quantidade: number,
+    public observacao?: string,
+  ) { }
+
+  get Id() { return String(this.id) }
 
   set Id(valor: string) {
+    if (this.id) throw new Error("Não é possível atribuir um novo id ao item do pedido")
     this.id = valor
   }
 
-  get IdItemMenu() {
-    return this.idItemMenu
+  get Nome() {return this.nome}
+
+  set Nome(valor: string) {
+    this.nome = valor
   }
 
-  get Status() {
-    return this.status;
+  get IdItemMenu() { return this.idItemMenu }
+
+  get Status() { return this.status }
+
+  set Status(valor: StatusItemPedido) {
+    this.status = valor
   }
 
-  get Quantidade() {
-    return this.quantidade;
-  }
+  get Quantidade() { return this.quantidade }
 
   calcularSubtotal(): number {
-    return this.precoUnitario * this.quantidade;
+    return this.precoUnitario * this.quantidade
   }
 
   acrescentar(): void {
-    this.quantidade++;
+    this.quantidade++
   }
 
   reduzir(): void {
     if (this.quantidade <= 1) {
-      throw new Error("Quantidade não pode ser menor que 1");
+      throw new Error("Quantidade não pode ser menor que 1")
     }
-    this.quantidade--;
+    this.quantidade--
   }
 
   alterarStatus(status: StatusItemPedido) {
-    this.status = status;
+    this.status = status
   }
 }
