2388466c57be1134220e30cbd935c4f868b6eecd
André Alves <andre.fortes.alves06@gmail.com>
2026-03-30 12:57:12 -0300

    feat: adiciona o Docker à aplicação

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
-import styles from "./page.module.css"
+"use client"
 
 export default function Home() {
+  function get() {
+    fetch('/api/pedidos?status=ABERTO')
+    .then(res => res.json())
+    .then(res => console.log(res))
+  }
   return (
-    <></>
+    <>
+      <button onClick={get}>Fetch</button>
+    </>
   )
 }
