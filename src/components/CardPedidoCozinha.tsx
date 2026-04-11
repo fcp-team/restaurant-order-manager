705afaef4ac65461ad58fe63a881932d8f88d269
Stephanny Borrozzino <154994245+StephannyBorrozzino@users.noreply.github.com>
2026-04-11 15:01:42 -0300

    conexão com a api para mostrar os pedidos na cozinha (#20)

--- src/components/CardPedidoCozinha.tsx
+++ src/components/CardPedidoCozinha.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect} from "react"
 
 type CardCozinhaProps = {
     pedido: Record<string, any>
@@ -6,13 +6,17 @@ type CardCozinhaProps = {
 
 export default function CardPedidoCozinha({pedido}: CardCozinhaProps){
     
-     const [itens, setItens] = useState(pedido.itens)
+     const [itens, setItens] = useState<any[]>([]);
 
-    function mudarEstado() {
+     useEffect(() => {
+        setItens(pedido.itens || []);
+    }, [pedido]);
+
+    async function mudarEstado() {
        
             const novosItens = itens.map((item: any) => ({
             ...item,
-            status: "Finalizado"
+            status: "FINALIZADO"
         }))
 
         setItens(novosItens);
@@ -24,17 +28,34 @@ export default function CardPedidoCozinha({pedido}: CardCozinhaProps){
         }))
         };
 
-         fetch("/api/pedido/itens/atualizar-status", {method: 'PATCH',headers: {"Content-Type": 'application/json'}, body: JSON.stringify(payload)})
+         try {
+        const res = await fetch("/api/pedido/itens/atualizar-status", {
+            method: 'PATCH',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(payload)
+        });
+
+        if (!res.ok) {
+            throw new Error("Falha ao atualizar no servidor");
+        }
+        
+        console.log("Status atualizado com sucesso!");
+        } catch (err) {
+            console.error(err);
+        }
       
     }
 
     return (
         <div style={{width: 430}} className="bg-[var(--color-surface)] border-[var(--color-surface-border)] border-[var(--color-button-action-border)] border-2 flex flex-col rounded-2xl p-4 m-4">
-            <h3 className="text-4xl">Pedido n°{pedido.numeroPedido} - Mesa {pedido.numeroMesa}</h3>
-            <p className="pt-2">{pedido.data}</p>
+            <h3 className="text-4xl">Pedido n°{pedido.id} - Mesa {pedido.numeroMesa}</h3>
+            <p className="pt-2">{new Date(pedido.criadoEm).toLocaleString()}</p>
             <ul className="text-2xl list-disc list-inside m-3">
-                {itens?.map((item: string, index: number) => (
-                    <li key={index}>{item}</li>
+                {itens.map((item: any) => (
+                    <li key={item.id} className="mb-4">
+                    {item.quantidade}x {item.nome} - {item.status}
+                    {item.observacao && ` (${item.observacao})`}
+                    </li>
                 ))}
             </ul>
             <button onClick={mudarEstado} className="cursor-pointer bg-[var(--color-button-action)] transition duration-300 hover:bg-[var(--color-button-action-hover)] border-[var(--color-button-action-border)] border-2 rounded-2xl p-2 px-5 m-3">
