11b75cc5e21745673f933d3f2c6e1ac0e7d69cc8
André Alves <andre.fortes.alves06@gmail.com>
2026-05-02 13:27:59 -0300

    feat: adiciona página de registro do administrador (único acesso)

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,15 +1,33 @@
 import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import { autorizar, verificarToken } from "./lib/auth";
+import ServicoUsuario from "./server/services/usuario.servico";
+import { RepositorioUsuario } from "./server/repositories/usuario.repositorio";
+import { Funcao } from "./lib/enums/funcao";
 
 export async function proxy(request: NextRequest) {
   try {
+    const { pathname } = request.nextUrl
+
+    if (
+      pathname.startsWith("/cadastro") ||
+      pathname.startsWith("/api/auth/cadastrar")
+    ) {
+      const admins = await new ServicoUsuario(new RepositorioUsuario())
+        .listarPorFuncao(Funcao.ADMIN) || []
+
+      if (admins.length > 0) return NextResponse.json(
+        { error: "Recurso indisponível" },
+        { status: 404 }
+      )
+      return NextResponse.next()
+    }
+
     const cookieStore = await cookies()
     const token = cookieStore.get("auth_token")?.value || ""
 
     const usuario = verificarToken(token)
 
-    const { pathname } = request.nextUrl
 
     if (pathname.startsWith("/api/pedidos")) {
       return NextResponse.next()
@@ -39,6 +57,7 @@ export const config = {
     "/api/pedido/:path*",
     "/api/pedidos",
     "/api/usuarios/:path*",
-    // "/api/auth/registrar"
+    "/api/auth/cadastrar",
+    "/cadastro"
   ]
 }
