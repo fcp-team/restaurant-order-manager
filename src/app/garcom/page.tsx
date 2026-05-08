6285fbf479c6d18ad78195f9b56db02453ddf370
André Alves <andre.fortes.alves06@gmail.com>
2026-05-08 18:45:45 -0300

    feat: adiciona manipulador para criação de pedidos e ajusta estilos da página

--- src/app/garcom/page.tsx
+++ src/app/garcom/page.tsx
@@ -1,12 +1,33 @@
 "use client"
-import { useState, useEffect } from "react"
+import { useState, useEffect, SubmitEvent } from "react"
 import Header from "@/components/Header"
 import CardPedidoGarcom from "@/components/CardPedidoGarcom"
 import { getSocket } from "@/lib/ws-client"
 import { ItemMenuDTO, MenuDTO } from "@/lib/dtos/menu"
 import { Pedido } from "@/server/classes/pedido"
-import { NovoItemPedidoPayload } from "@/lib/dtos/pedido"
+import { NovoItemPedidoPayload, NovoPedidoPayload } from "@/lib/dtos/pedido"
 
+// remove later
+const test = []
+for (let i = 0; i <= 10; i++) {
+  test.push(
+    <li
+      key={i}
+      className="grid grid-cols-[auto_3fr_1fr] items-center gap-4 w-full py-4 px-6 bg-white hover:bg-gray-100">
+      <input
+        type="checkbox"
+        className="size-5"
+      />
+
+      <div>
+        <p className="text-lg font-medium">Teste</p>
+        <p className="text-sm text-gray-600">Teste</p>
+      </div>
+
+      <span>R$ 100,00</span>
+    </li>
+  )
+}
 
 function PedidoForm() {
   const [menus, setMenus] = useState<MenuDTO[]>([])
@@ -84,74 +105,113 @@ function PedidoForm() {
 
   useEffect(() => console.log(itensPedido), [itensPedido])
 
-  async function criarPedido() {
+  async function handleSubmit(event: SubmitEvent<HTMLFormElement>) {
+    event.preventDefault()
+    if (!itensPedido.length) {
+      alert("Não há itens no pedido.")
+      return
+    }
 
+    const formData = new FormData(event.currentTarget)
+    const numeroMesa = String(formData.get("numero-mesa"))
+    if (!numeroMesa) {
+      alert("O número da mesa deve ser indicado.")
+      return
+    }
+
+    const pedido: NovoPedidoPayload = {
+      numeroMesa: numeroMesa,
+      itens: itensPedido
+    }
+
+    console.log(pedido)
+
+    try {
+      const res = await fetch("/api/pedido/criar", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(pedido)
+      })
+
+      if (!res.ok) {
+        alert("Houve um problema ao registrar o pedido: " + res.statusText)
+        return
+      }
+    } catch (reason) {
+      console.error(reason)
+      alert("Houve um problema ao registrar o pedido: " + (reason as Error).message)
+    }
   }
 
   return (
     <div className="w-xl flex flex-col items-center p-8 mx-auto">
       <h2 className="text-3xl font-bold mb-8 self-start">Adicionar Pedidos</h2>
 
-      <input
-        type="number"
-        inputMode="numeric"
-        min={0}
-        placeholder="Nº mesa"
-        className="w-full max-w-lg border border-gray-300 rounded-lg p-3 mb-4"
-      />
-
-      {itensSelecionados.length > 0 && (
-        <ul className="flex flex-col gap-px w-full overflow-hidden rounded-2xl bg-gray-500 shadow">
-          {itensSelecionados.map((item, index) => (
-            <li
-              key={index}
-              className="grid grid-cols-[4fr_1fr_auto_auto] items-center gap-4 w-full py-4 px-6 bg-white hover:bg-gray-100">
-              <div>
-                <p className="text-lg font-medium">{item.nome}</p>
-                <input
-                  type="text"
-                  placeholder="Adicionar observação"
-                  onChange={(e) => adicionarObservacao(item.id, e.target.value)}
-                  className="w-full px-3 py-1 border rounded-md text-sm"
-                />
-              </div>
-
-              <span>
-                {new Intl.NumberFormat('pt-BR', {
-                  style: 'currency',
-                  currency: 'BRL',
-                }).format(item.preco)}
-              </span>
-
-              <input
-                type="number"
-                inputMode="numeric"
-                min={1}
-                max={99}
-                defaultValue={1}
-                onChange={(e) => alterarQuantidade(item.id, Number(e.target.value))}
-                className="w-min text-2xl text-center"
-              />
-
-              <button
-                type="button"
-                onClick={() => removerItem(item.id)}
-                className="text-2xl text-red-600"
-              >
-                &times;
-              </button>
-            </li>
-          ))}
-        </ul>
-      )}
-
-      <button
-        type="button"
-        onClick={criarPedido}
-        className="cursor-pointer bg-(--color-button-action) border-(--color-button-action-border) border-2 rounded-2xl p-2 px-5 w-full max-w-lg mb-4"
+      <form
+        onSubmit={(e) => handleSubmit(e)}
+        className="flex flex-col items-center gap-4 w-full"
       >
-        Criar pedido
-      </button>
+        <input
+          type="number"
+          name="numero-mesa"
+          required
+          inputMode="numeric"
+          min={0}
+          placeholder="Nº mesa"
+          className="w-full max-w-lg border border-gray-400 rounded-lg p-3 bg-white"
+        />
+
+        {itensSelecionados.length > 0 && (
+          <ul className="flex flex-col gap-px w-full overflow-hidden rounded-2xl bg-gray-500 shadow">
+            {itensSelecionados.map((item, index) => (
+              <li
+                key={index}
+                className="grid grid-cols-[4fr_1fr_auto_auto] items-center gap-4 w-full py-4 px-6 bg-white hover:bg-gray-100">
+                <div>
+                  <p className="text-lg font-medium">{item.nome}</p>
+                  <input
+                    type="text"
+                    placeholder="Adicionar observação"
+                    onChange={(e) => adicionarObservacao(item.id, e.target.value)}
+                    className="w-full px-3 py-1 border rounded-md text-sm"
+                  />
+                </div>
+
+                <span>
+                  {new Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                  }).format(item.preco)}
+                </span>
+
+                <input
+                  type="number"
+                  inputMode="numeric"
+                  min={1}
+                  max={99}
+                  defaultValue={1}
+                  onChange={(e) => alterarQuantidade(item.id, Number(e.target.value))}
+                  className="w-min text-2xl text-center"
+                />
+
+                <button
+                  type="button"
+                  onClick={() => removerItem(item.id)}
+                  className="text-2xl text-red-600"
+                >
+                  &times;
+                </button>
+              </li>
+            ))}
+          </ul>
+        )}
+        <button
+          type="submit"
+          className="cursor-pointer bg-(--color-button-action) border-(--color-button-action-border) border-2 rounded-2xl p-2 px-5 w-full max-w-lg mb-4"
+        >
+          Criar pedido
+        </button>
+      </form>
 
       {/* Cardápio */}
       <input
@@ -159,10 +219,10 @@ function PedidoForm() {
         placeholder="Pesquisar item"
         value={filtroItem}
         onChange={(e) => setFiltroItem(e.target.value)}
-        className="w-full max-w-lg border border-gray-300 rounded-lg p-3 mb-4"
+        className="w-full max-w-lg border border-gray-400 rounded-lg p-3 mb-4 bg-white"
       />
 
-      <ul className="flex flex-col gap-px w-full overflow-hidden rounded-2xl bg-gray-500 shadow">
+      <ul className="flex flex-col gap-px w-full max-h-[60dvh] overflow-auto rounded-2xl bg-gray-500 shadow">
         {itensFiltrados.map((item, index) => (
           <li
             key={index}
